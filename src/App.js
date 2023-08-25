eb59ee424a3d16ea57030a6bc504959f64625d54
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2023-08-26 01:18:47 +0300

    hero

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import Header from './components/Header';
 
 import './App.css';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
 
       <Header />
+
+      <Hero />
+
 
     </div>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+
+export default function Hero () {
+    return (
+        <div id='Hero'>
+    
+            <div className="hero__content">
+            <h2>Struggling with your buisness Online?</h2>
+            <p>
+                Welcome to the write place, we are here to get your started on your <strong>Google Buisness Profile</strong>, from claiming your buisness to optomizing to rank highier than your competetors. Get started with our service now.
+            </p>
+            <a>Contact Now</a>
+            </div>
+        
+      </div>
+    )
+}
